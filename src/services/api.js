44c7e3d961539683f1cb0e882a33b8b3948ab356
prajwal-manjunath20 <prajwalmanjunath95@gmail.com>
2026-04-17 17:57:04 +0530

    restore dynamic api url parsing for vercel deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = '/api';
+const BASE_URL = import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : '/api';
 
 const api = axios.create({ baseURL: BASE_URL });
 
